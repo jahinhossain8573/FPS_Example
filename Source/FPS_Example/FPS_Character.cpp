049c13b1698ecec75510107546a17e81aa228b86
JAHIN\Jahin <jahin.hossain2006@gmail.com>
2022-06-20 13:44:02 +0600

    Progressed on the calculation of the Aim Offset Changed the idle animation for aiming from AK74 to M4A1

--- Source/FPS_Example/FPS_Character.cpp
+++ Source/FPS_Example/FPS_Character.cpp
@@ -30,7 +30,7 @@ void AFPS_Character::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
-	AimOffset = GunMesh->GetSocketLocation("AimSocket") - GunMesh->GetSocketLocation("CameraSocket") + GunMesh->GetSocketLocation("hand_r");
+	AimOffset = GunMesh->GetSocketLocation("AimSocket") - GunMesh->GetSocketLocation("CameraSocket");
 }
 
 void AFPS_Character::MoveForward(float Value)
